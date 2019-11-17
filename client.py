a01643aab19c91c9b9ede94d1f52a693f2e8a333
janghyeonan <ajh910@gmail.com>
2019-11-17 22:25:18 +0900

    내용 변경1
    ㅇㅇ

---
+++ client.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  8 14:33:08 2019
+
+@author: ajh910 
+"""
+import socket 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options 
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities   
+import time
+import os
+import datetime
+
+##윗 PC
+url = 'https://blog.naver.com/ajh910'
+driver_path = 'c:\\renardy\\chromedriver.exe'
+
+
+#크롬세팅
+_chrome_options = Options() 
+_chrome_options.add_argument('disable-infobars') 
+d = DesiredCapabilities.CHROME
+d['loggingPrefs'] = { 'browser':'ALL' }
+
+#소켓접속주소
+HOST = '192.168.0.12'
+PORT = 9997
+
+#소켓연결
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+client_socket.connect((HOST, PORT)) 
+
+#서버에게 메시지 전달 
+def s_msg(text):
+    print(text)
+    global client_socket
+    client_socket.send(text.encode())
+    
+
+print('- 클라이언트 서버에 접속 완료!\n수신 대기중 ...')
+
+while True: 
+    msg = client_socket.recv(1024).decode()        
+    print('받은 메시지 : '+msg)
+    
+    if msg == '테스트시작':        
+        #글로벌 변수선언
+        driver = webdriver.Chrome(driver_path,  chrome_options=_chrome_options)
+        s_msg('클라이언트 세팅 완료')
+        
+    elif msg == '테스트종료':
+        driver.quit()
+        
+    elif msg == '연결종료':        
+        break
+        
+    elif msg =='스샷':
+        try:
+            nnow = datetime.datetime.now().strftime("%y%m%d%H%M%S")
+            driver.save_screenshot(r'c:\\renardy\\'+nnow+'_screenshot.png')
+                        
+            s_msg('스크린샷 완료')        
+        except Exception as ex:
+            print('에러가 발생 했습니다8\n', ex)
+        
+    else: #설정된 메시지 외 다른게 들어왔을 때
+        s_msg('명령어 목록이 없습니다.')
+         
+client_socket.close()
+print('소켓 연결이 종료되었습니다.')
+time.sleep(3)
+os._exit(0)
---
+++ patch_client.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  8 14:33:08 2019
+
+@author: ajh910 
+"""
+import socket 
+import requests
+import subprocess
+import os
+import time
+
+print('패치 서버 접속 대기중...')
+#소켓접속주소
+HOST = '192.168.0.12'
+PORT = 9999
+#소켓연결
+
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+client_socket.connect((HOST, PORT)) 
+
+
+#서버에게 메시지 전달 
+def s_msg(text):
+    print(text)
+    global client_socket
+    client_socket.send(text.encode())
+  
+print('- 클라이언트 서버에 접속 완료!\n수신 대기중 ...')
+
+while True: 
+    try:
+        msg = client_socket.recv(1024).decode()        
+        print('받은 메시지 : '+msg)
+        
+        if msg == '패치시작':
+            url = '깃허브'
+            r = requests.get(url, allow_redirects=True)
+            open(r'c:\\renardy\\client.py', 'wb').write(r.content)
+            print('클라 다운로드 완료!')
+            
+            url2 = '깃허브'
+            r2 = requests.get(url2, allow_redirects=True)
+            open(r'c:\\renardy\\patch_client.py', 'wb').write(r2.content)
+            print('패치클라이언 완료!')
+        elif msg =='클라실행':
+            subprocess.Popen(r'c:\\renardy\\remote_client.bat', creationflags=subprocess.CREATE_NEW_CONSOLE)
+        elif msg =='연결종료':
+            break
+            
+    except Exception as ex:
+        print('에러가 발생 했습니다', ex)
+
+client_socket.close()
+print('소켓 연결이 종료되었습니다.')
+time.sleep(3)
+os._exit(0)
